e9282a2c620ca082c1dce0b03de6a70d566156a5
eraldo <eraldo.borel@infoglobo.com.br>
2018-02-16 20:08:57 -0200

    Criação de model para planeta

---
+++ src/main/java/br/com/eraldoborel/starwarsplanets/model/Planeta.java
@@ -0,0 +1,52 @@
+package br.com.eraldoborel.starwarsplanets.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "planeta")
+public class Planeta {
+
+	@Id
+    private long id;
+	
+	@Indexed(unique = true)
+	private String nome;
+	
+	private String clima;
+	
+	private String terreno;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getClima() {
+		return clima;
+	}
+
+	public void setClima(String clima) {
+		this.clima = clima;
+	}
+
+	public String getTerreno() {
+		return terreno;
+	}
+
+	public void setTerreno(String terreno) {
+		this.terreno = terreno;
+	}
+}
