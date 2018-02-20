051f517e22a884e47b5006540bcba6207604e517
eraldo <eraldo.borel@infoglobo.com.br>
2018-02-20 16:33:11 -0300

    Limpeza de código

--- src/main/java/br/com/eraldoborel/starwarsplanets/StarWarsPlanetsApplication.java
+++ src/main/java/br/com/eraldoborel/starwarsplanets/StarWarsPlanetsApplication.java
@@ -3,8 +3,6 @@ package br.com.eraldoborel.starwarsplanets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class StarWarsPlanetsApplication {
 	
